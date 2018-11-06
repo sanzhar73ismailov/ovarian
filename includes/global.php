3ab6be58b42ffe24f3317a1f3fa2f0bcac2e353b
sanzhar.ismailov <sanzhar73@gmail.com>
2018-11-06 10:24:48 +0100

    send to test email - off

--- includes/global.php
+++ includes/global.php
@@ -9,7 +9,7 @@ define('ADMIN_EMAIL', "sanzhar73@mail.ru");
 define('SENDER_EMAIL', "sanzhar73@mail.ru");
 define('GENERATE_TEST_OBJECTS', "0"); // 1 - генерировать тестовые объекты для форм, 0 - не генерировать
 define('ZAGLUSHKA_MAIL', "0"); // 1 - Заглушка для работы почты (отправляться не будет), 0 - почта будет отправляться
-define('SEND_MAIL_TO_TEST_ADDR', "1"); // 1 - Отправка на тестовые email адреса, 0 - отправка на настоящие email адреса
+define('SEND_MAIL_TO_TEST_ADDR', "0"); // 1 - Отправка на тестовые email адреса, 0 - отправка на настоящие email адреса
 define(MAIL_P, "IliveinKrakow+");
 define('TEST_EMAILS', "javajan@mail.ru,sanzhar@yahoo.com"); // тестовые адреса, разделенные запятой, используются если SEND_MAIL_TO_TEST_ADDR=1 
 define('TEST_USER_EMAIL', "javajan@mail.ru"); // тестовые адрес пользователя, используется если SEND_MAIL_TO_TEST_ADDR=1
